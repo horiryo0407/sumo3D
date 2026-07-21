6e08aaaa2abad3852b5519770451016d4af4543c
湊斗 管野 <240579@jc-21.jp>
2026-07-21 14:03:58 +0900

    えふぇ

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,60 +14,42 @@ public class PlayerController : MonoBehaviour
     private Vector3 dashDirection;
     private Vector3 moveInput;
 
-
-    // 剛掌波用
+    // 剛掌波
     private bool isGoshoCharging = false;
-
     public float goshoChargeLimit = 3f;
     private float goshoTimer = 0f;
 
     public GameObject goshoBeamPrefab;
     public Transform goshoPoint;
-
-
+    public GameObject chargeBall;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponentInChildren<Animator>();
+
+        if (chargeBall != null)
+            chargeBall.SetActive(false);
     }
-
-
 
     void Update()
     {
         // パンチ
         if (Input.GetKeyDown(KeyCode.F))
+            animator.SetTrigger("Attack");
+
+        // 剛掌波チャージ
+        if (Input.GetKeyDown(KeyCode.O) && !isGoshoCharging)
         {
-            if (animator != null)
-            {
-                animator.SetTrigger("Attack");
-            }
+            isGoshoCharging = true;
+            goshoTimer = goshoChargeLimit;
+            animator.SetTrigger("Gosho");
+
+            if (chargeBall != null)
+                chargeBall.SetActive(true);
         }
 
-
-
-        // Oで剛掌波溜め開始
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            if (!isGoshoCharging)
-            {
-                isGoshoCharging = true;
-
-                goshoTimer = goshoChargeLimit;
-
-                if (animator != null)
-                {
-                    animator.SetTrigger("Gosho");
-                }
-
-                Debug.Log("剛掌波 溜め開始");
-            }
-        }
-
-
-
-        // 溜め時間カウント
+        // チャージ時間
         if (isGoshoCharging)
         {
             goshoTimer -= Time.deltaTime;
@@ -76,189 +58,81 @@ public class PlayerController : MonoBehaviour
             {
                 isGoshoCharging = false;
 
-                Debug.Log("剛掌波キャンセル");
+                if (chargeBall != null)
+                    chargeBall.SetActive(false);
             }
         }
 
-
-
-        // Pで発射
-        if (Input.GetKeyDown(KeyCode.P))
+        // 発射
+        if (Input.GetKeyDown(KeyCode.P) && isGoshoCharging)
         {
-            if (isGoshoCharging)
-            {
-                FireGoshoBeam();
+            FireGoshoBeam();
 
-                isGoshoCharging = false;
+            isGoshoCharging = false;
 
-                Debug.Log("剛掌波 発射！");
-            }
+            if (chargeBall != null)
+                chargeBall.SetActive(false);
         }
 
+        if (Input.GetKeyDown(KeyCode.UpArrow)) animator.SetTrigger("EmoteUp");
+        if (Input.GetKeyDown(KeyCode.DownArrow)) animator.SetTrigger("EmoteDown");
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) animator.SetTrigger("EmoteLeft");
+        if (Input.GetKeyDown(KeyCode.RightArrow)) animator.SetTrigger("EmoteRight");
 
+        float hPad = Input.GetAxisRaw("Horizontal");
+        float vPad = Input.GetAxisRaw("Vertical");
 
-        // エモート
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        float hKb = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+        float vKb = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+
+        float h = Mathf.Abs(hPad) > 0.1f ? hPad : hKb;
+        float v = Mathf.Abs(vPad) > 0.1f ? vPad : vKb;
+
+        moveInput = new Vector3(-h, 0, -v).normalized;
+
+        bool dash = Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.JoystickButton0);
+
+        if (!isDashing && moveInput.magnitude > 0.1f && dash)
         {
-            animator.SetTrigger("EmoteUp");
+            isDashing = true;
+            dashTimer = dashDuration;
+            dashDirection = moveInput;
         }
-
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            animator.SetTrigger("EmoteDown");
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            animator.SetTrigger("EmoteLeft");
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            animator.SetTrigger("EmoteRight");
-        }
-
-
-
-
-        // コントローラー入力
-        float h_pad = Input.GetAxisRaw("Horizontal");
-        float v_pad = Input.GetAxisRaw("Vertical");
-
-
-        float h_kb = 0f;
-        float v_kb = 0f;
-
-        if (Input.GetKey(KeyCode.A)) h_kb = -1f;
-        if (Input.GetKey(KeyCode.D)) h_kb = 1f;
-        if (Input.GetKey(KeyCode.W)) v_kb = 1f;
-        if (Input.GetKey(KeyCode.S)) v_kb = -1f;
-
-
-        float h = (Mathf.Abs(h_pad) > 0.1f) ? h_pad : h_kb;
-        float v = (Mathf.Abs(v_pad) > 0.1f) ? v_pad : v_kb;
-
-
-        moveInput = new Vector3(-h, 0f, -v).normalized;
-
-
-
-        // ダッシュ
-        bool isDashPressed =
-            Input.GetKeyDown(KeyCode.JoystickButton0) ||
-            Input.GetKeyDown(KeyCode.LeftShift);
-
-
-        if (!isDashing && moveInput.magnitude > 0.1f && isDashPressed)
-        {
-            StartDash(moveInput);
-        }
-
-
 
         if (isDashing)
         {
             dashTimer -= Time.deltaTime;
-
-            if (dashTimer <= 0f)
-            {
-                isDashing = false;
-            }
+            if (dashTimer <= 0) isDashing = false;
         }
     }
 
-
-
-
-
-    // 剛掌波発射
     void FireGoshoBeam()
     {
         if (goshoBeamPrefab != null && goshoPoint != null)
-        {
-            Instantiate(
-                goshoBeamPrefab,
-                goshoPoint.position,
-                goshoPoint.rotation
-            );
-        }
+            Instantiate(goshoBeamPrefab, goshoPoint.position, goshoPoint.rotation);
     }
-
-
-
-
-
-    void StartDash(Vector3 direction)
-    {
-        isDashing = true;
-
-        dashTimer = dashDuration;
-
-        dashDirection = direction;
-    }
-
-
-
-
 
     void FixedUpdate()
     {
-        float h_pad = Input.GetAxisRaw("Horizontal");
-        float v_pad = Input.GetAxisRaw("Vertical");
+        float hPad = Input.GetAxisRaw("Horizontal");
+        float vPad = Input.GetAxisRaw("Vertical");
+        float hKb = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+        float vKb = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
 
+        float h = Mathf.Abs(hPad) > 0.1f ? hPad : hKb;
+        float v = Mathf.Abs(vPad) > 0.1f ? vPad : vKb;
 
-        float h_kb = 0f;
-        float v_kb = 0f;
-
-
-        if (Input.GetKey(KeyCode.A)) h_kb = -1f;
-        if (Input.GetKey(KeyCode.D)) h_kb = 1f;
-        if (Input.GetKey(KeyCode.W)) v_kb = 1f;
-        if (Input.GetKey(KeyCode.S)) v_kb = -1f;
-
-
-
-        float finalH = (Mathf.Abs(h_pad) > 0.1f) ? h_pad : h_kb;
-        float finalV = (Mathf.Abs(v_pad) > 0.1f) ? v_pad : v_kb;
-
-
-
-        float moveAmount = Mathf.Abs(finalH) + Mathf.Abs(finalV);
-
-
-
-        if (animator != null)
-        {
-            animator.SetFloat("Speed", moveAmount);
-        }
-
-
+        animator.SetFloat("Speed", Mathf.Abs(h) + Mathf.Abs(v));
 
         if (isDashing)
-        {
-            rb.MovePosition(
-                rb.position + dashDirection * dashSpeed * Time.fixedDeltaTime
-            );
-
-            return;
-        }
-
-
-
-        rb.MovePosition(
-            rb.position + moveInput * speed * Time.fixedDeltaTime
-        );
+            rb.MovePosition(rb.position + dashDirection * dashSpeed * Time.fixedDeltaTime);
+        else
+            rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
     }
-
-
-
-
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "Enemy")
-        {
             Debug.Log("敵に当たった！");
-        }
     }
 }
